1f72ec00c492c41a2a23b1fe8effe08a15a0ee8e
dudemaz <maznicenkomisa@gmail.com>
2026-04-25 22:31:02 +0300

    feat: full logic api request

--- js/api/api.js
+++ js/api/api.js
@@ -1,9 +1,9 @@
-import { domEl } from '../ui/dom/domel';
-let city = domEl.searchInput.value;
+// import { domEl } from '../ui/dom/domel';
+// let city = domEl.searchInput.value;
 export async function GetCitylatlon(city) {
   try {
-    const api_key = '';
-    const url = ``;
+    const API_KEY = 'api';
+    const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`;
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -12,26 +12,24 @@ export async function GetCitylatlon(city) {
     if (data.length === 0) {
       throw new Error('City not found');
     }
-    console.log(data);
     return { lat: data[0].lat, lon: data[0].lon };
   } catch (error) {
     throw new Error('error dosnt work funct');
   }
 }
+let data = await GetCitylatlon('Москва');
 export async function LoadWeatherData(lat, lon) {
   try {
-    const API_KEY = '42f90f490fde6bd9aa84073cd6d3f5ee';
+    const API_KEY = 'api';
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     throw new Error('Failed to get weather data: ' + error.message);
   }
 }
-
-LoadWeatherData(GetCitylatlon('London').lat, GetCitylatlon('London').lon);
+console.log(await LoadWeatherData(data.lat, data.lon));
